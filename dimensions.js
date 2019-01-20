ae241d74a819ed6b3cfdba803d0b370535318daa
Yazaar <41300736+Yazaar@users.noreply.github.com>
2019-01-20 16:19:53 +0100

    Uploaded required files for Slim version

---
+++ dimensions.js
@@ -0,0 +1,11 @@
+// height and width of overlay, make it whatever shape you want! Remember that you can crop it in OBS as well! Font size and more auto calculated.
+var OverlayHeight = "100px"
+var OverlayWidth = "400px"
+
+
+// artist font size related to track font size (0.5 = 50% smaller than track font size)
+// number between -1 and 1 (-1 = Only artist and 1 = Only track)
+var FontSizeDiff = 0.3
+
+// the movement speed of the ticker in pixels per second (both track and artist ticker) (lower = slower)
+var PixelsPerSecond = 50
